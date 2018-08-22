80a22837074eff3098d240d8c316f2c1b5e2730e
Manuel Pissanos <mpissano@gmail.com>
2018-08-22 16:17:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function theBeatlesPlay(musicians, instruments) {
 
 function johnLennonFacts(facts) {
   while(facts.length > 0) {
-     facts[i] + "!!!"; 
+     facts + "!!!"; 
   }
   return facts;
 }
