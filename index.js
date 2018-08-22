28a0af32af8ff13a2b383342d5f3bb6b81f95254
Manuel Pissanos <mpissano@gmail.com>
2018-08-22 15:37:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var myArr = []
+  for( var i = 0; i < musicians.length; i++) {
+    myArr.push(musicians[i]+ ' plays '+(instruments[i]))
+  }
+  return myArr;
+}
+
+function johnLennonFacts(array) {
+  while(i < array.length) {
+    [i] + '!!!';
+  }
+}
